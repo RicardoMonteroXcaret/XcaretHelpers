fbb381a86f5d22fd725bd385908196b9792925b9
RicardoMonteroXcaret <136732450+RicardoMonteroXcaret@users.noreply.github.com>
2023-08-07 15:14:36 -0600

    Agregar archivos de proyecto.

---
+++ TestConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Xcaret.ClientHTTP;
+using Xcaret.ClientHTTP.ClientHttpWrapper;
+using XcaretHelpers;
+
+internal class Program 
+{
+    private IClientFactory _clientFactory;
+
+    public Program(IClientFactory clientFactory)
+    {
+        _clientFactory = clientFactory;
+    }
+
+    private static void Main(string[] args)
+    {
+        Test test = new();
+
+        test.Hi();
+
+        Console.WriteLine("Hello, World!" + test.Hi());
+    }
+}
---
+++ Xcaret.ClientHTTP/ClientHttpWrapper/ClientFactory.cs
@@ -0,0 +1,212 @@
+﻿using System.Net.Http;
+using System.Text;
+using Xcaret.ClientHTTP.ClientHttpWrapper;
+
+namespace Xcaret.ClientHTTP.ClientHttpWrapper
+{
+    /// <summary>
+    /// Cliente para acceso a servicios
+    /// </summary>
+    public class ClientFactory : IClientFactory
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        /// <summary>
+        /// Client Factory Contructor
+        /// </summary>
+        /// <param name="httpClientFactory"></param>
+        public ClientFactory(IHttpClientFactory httpClientFactory)
+            => _httpClientFactory = httpClientFactory;
+        /// <summary>
+        /// Get Async
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="tagHttpClient"></param>
+        /// <param name="url"></param>
+        /// <param name="headers"></param>
+        /// <param name="serializeFormat"></param>
+        /// <returns></returns>
+        public async Task<TResult> GetAsync<TResult>(string tagHttpClient,
+            string url,
+            Dictionary<string, string> headers,
+            SerializeFormat serializeFormat)
+        {
+            var httpClient = _httpClientFactory.CreateClient(tagHttpClient);
+            //Creating get request
+            var requestGet = new HttpRequestMessage(HttpMethod.Get, url);
+
+            SetHeaders(requestGet, headers);
+
+            //var headersLogR = string.Empty;
+            //if (headers != null
+            //    && headers.Any())
+            //{
+            //    //httpClient.DefaultRequestHeaders.Authorization
+            //    //         = new AuthenticationHeaderValue("Basic", headers["Authorization"]);
+            //    //headersLogR = string.Join(",", headers.Select(k => $"{k.Key}, {k.Value}"));
+            //}
+
+            //Serilog.Log.Error($"Url: {url} | Headers: [{headersLogR}]");
+
+            var result = await httpClient.SendAsync(requestGet);//await httpClient.GetAsync(url);
+
+            if (!result.IsSuccessStatusCode)
+            {
+                var headersLog = string.Empty;
+                if (headers != null
+                    && headers.Any())
+                {
+                    headersLog = string.Join(",", headers.Select(k => $"{k.Key}, {k.Value}"));
+                }
+                throw new HttpRequestException($"Url: {url} | Headers: [{headersLog}] | {await result.Content.ReadAsStringAsync()}");
+            }
+            return (await result.Content.ReadAsStreamAsync()).ToEntity<TResult>(serializeFormat);
+        }
+        /// <summary>
+        /// Post
+        /// </summary>
+        /// <typeparam name="TRequest"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="tagHttpClient"></param>
+        /// <param name="url"></param>
+        /// <param name="request"></param>
+        /// <param name="headers"></param>
+        /// <param name="serializeFormat"></param>
+        /// <returns></returns>
+        public async Task<TResult> PostAsync<TRequest, TResult>(string tagHttpClient,
+            string url,
+            TRequest request,
+            Dictionary<string, string> headers,
+            SerializeFormat serializeFormat)
+        {
+            var httpClient = _httpClientFactory.CreateClient(tagHttpClient);
+
+            var requestPost = new HttpRequestMessage()
+            {
+                RequestUri = new Uri(url),
+                Method = HttpMethod.Post
+            };
+
+            var requestStr = RequestToStringContent(request, serializeFormat);
+
+            requestPost.Content = new StringContent(requestStr,
+                Encoding.UTF8, SetContenType(serializeFormat));
+
+            SetHeaders(requestPost, headers);
+            var result = await httpClient.SendAsync(requestPost);
+
+            if (!result.IsSuccessStatusCode)
+            {
+                var headersLog = string.Empty;
+                if (headers != null
+                    && headers.Any())
+                {
+                    headersLog = string.Join(",", headers.Select(k => $"{k.Key}, {k.Value}"));
+                }
+                throw new HttpRequestException($"Request: {requestStr} | Headers: [{headersLog}] | {await result.Content.ReadAsStringAsync()}");
+            }
+            return (await result.Content.ReadAsStreamAsync()).ToEntity<TResult>(serializeFormat);
+        }
+
+        private static string SetContenType(SerializeFormat serializeFormat)
+            => serializeFormat == SerializeFormat.Xml ? "text/xml" : "application/json";
+
+        /// <summary>
+        /// Put
+        /// </summary>
+        /// <typeparam name="TRequest"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="tagHttpClient"></param>
+        /// <param name="url"></param>
+        /// <param name="request"></param>
+        /// <param name="headers"></param>
+        /// <param name="serializeFormat"></param>
+        /// <returns></returns>
+        public async Task<TResult> PutAsync<TRequest, TResult>(string tagHttpClient,
+            string url,
+            TRequest request,
+            Dictionary<string, string> headers,
+            SerializeFormat serializeFormat)
+        {
+            var httpClient = _httpClientFactory.CreateClient(tagHttpClient);
+            SetHeaders(httpClient, headers);
+
+            var requestStr = RequestToStringContent(request, serializeFormat);
+
+            var result = await httpClient.PutAsync(url, new StringContent(
+                requestStr, Encoding.UTF8));
+
+            if (!result.IsSuccessStatusCode)
+            {
+                var headersLog = string.Empty;
+                if (headers != null
+                    && headers.Any())
+                {
+                    headersLog = string.Join(",", headers.Select(k => $"{k.Key}, {k.Value}"));
+                }
+                throw new HttpRequestException($"Request: {requestStr} | Headers: [{headersLog}] | {await result.Content.ReadAsStringAsync()}");
+            }
+            return (await result.Content.ReadAsStreamAsync()).ToEntity<TResult>(serializeFormat);
+        }
+        /// <summary>
+        /// Remove
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="tagHttpClient"></param>
+        /// <param name="url"></param>
+        /// <param name="headers"></param>
+        /// <param name="serializeFormat"></param>
+        /// <returns></returns>
+        public async Task<TResult> RemoveAsync<TResult>(string tagHttpClient,
+            string url,
+            Dictionary<string, string> headers,
+            SerializeFormat serializeFormat)
+        {
+            var httpClient = _httpClientFactory.CreateClient(tagHttpClient);
+            SetHeaders(httpClient, headers);
+            //Creating get request
+            var result = await httpClient.DeleteAsync(url);
+
+            if (!result.IsSuccessStatusCode)
+            {
+                var headersLog = string.Empty;
+                if (headers != null
+                    && headers.Any())
+                {
+                    headersLog = string.Join(",", headers.Select(k => $"{k.Key}, {k.Value}"));
+                }
+                throw new HttpRequestException($"Url: {url} | Headers: [{headersLog}] | {await result.Content.ReadAsStringAsync()}");
+            }
+            return (await result.Content.ReadAsStreamAsync()).ToEntity<TResult>(serializeFormat);
+        }
+
+        private static string RequestToStringContent<TRequest>(TRequest request, SerializeFormat serializeFormat)
+        {
+            return serializeFormat == SerializeFormat.Json ?
+                                request.SerializeToJsonString() :
+                                request.SerializeToXmlString();
+        }
+
+        private void SetHeaders(HttpRequestMessage requestGet,
+            Dictionary<string, string> headers)
+        {
+            if (headers != null && headers.Any())
+            {
+                foreach (var header in headers)
+                {
+                    requestGet.Headers.Add(header.Key, header.Value);
+                }
+            }
+        }
+        private void SetHeaders(HttpClient requestGet,
+            Dictionary<string, string> headers)
+        {
+            if (headers != null && headers.Any())
+            {
+                foreach (var header in headers)
+                {
+                    requestGet.DefaultRequestHeaders.Add(header.Key, header.Value);
+                }
+            }
+        }
+    }
+}
